4fc0c656e59a4618ac386bc50be81c34e58c9793
Mickey <lweinste@Levins-MacBook-Air-2.local>
2017-04-08 10:55:38 -0700

    fixing

--- stop.py
+++ stop.py
@@ -1,9 +1,12 @@
 import subprocess, signal, os
-
-p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
-out, err = p.communicate()
+while True:
+	p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+	out, err = p.communicate()
 
 for line in out.splitlines():
 	if 'manager' in line:
 		pid = int(line.split(None, 1)[0])
+		os.kill(pid, signal.SIGKILL)
+	if 'yes' in line:
+		pid = int(line.split(none, 1)[0])
 		os.kill(pid, signal.SIGKILL)
